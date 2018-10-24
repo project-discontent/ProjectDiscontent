78a123b5d5e306d91843f8d54699c14dddb0aa34
Jonathan Turnbull-Reilly <j.reilly@sense.com>
2018-10-24 00:38:36 -0400

    Get contact form working.

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,43 +3,84 @@ import preact from 'preact';
 /** @jsx preact.h */
 
 
-const TextInput = ({ label }) =>
+const TextInput = ({ name, label, onChange }) =>
   <div class="field">
     <label class="white label is-size-7">{label}</label>
     <div class="control">
       <input
         class="input is-size-7 is-radiusless"
+        name={name}
+        onChange={onChange}
         type="text"
       />
     </div>
   </div>
 
-const TextArea = ({ label }) =>
+const TextArea = ({ name, label, onChange }) =>
   <div class="field">
     <label class="white label is-size-7">{label}</label>
     <div class="control">
       <input
         class="textarea is-size-7 is-radiusless"
+        name={name}
+        onChange={onChange}
         type="textarea"
       />
     </div>
   </div>
 
-const ContactForm = () =>
-  <form class="container" action="" style={{ maxWidth: 420 }}>
-    <TextInput label="Full Name" />
-    <br/>
-    <TextInput label="Email" />
-    <br/>
-    <TextInput label="Organization's website (optional)" />
-    <br/>
-    <TextArea label="Brief description of your need (optional)"/>
-    <br/>
-    <div class="has-text-centered">
-      <a class="is-danger has-text-dark button is-size-7 ">
-        Send →
-      </a>
-    </div>
-  </form>
+class ContactForm extends preact.Component {
+  state = {
+    name: "",
+    website: "",
+    needs: "",
+  }
+
+  setField = ({ target: { name, value }}) => {
+    this.setState({ [name]: value });
+  }
+
+  submit = (e) => {
+    e.preventDefault();
+    const { name, website, needs } = this.state;
+    const subject = encodeURIComponent(`Project Query | ${name} | ${website}`);
+    window.location.href = `mailto:project.discontent@gmail.com?subject=${subject}&body=${needs}`;
+  }
+
+  render () {
+    return (
+      <form class="container" action="" onSubmit={this.onSubmit} style={{ maxWidth: 420 }}>
+        <TextInput
+          label="Full Name"
+          name="name"
+          onChange={this.setField}
+         />
+        <br/>
+        <TextInput
+          label="Organization's website (optional)"
+          name="website"
+          onChange={this.setField}
+        />
+        <br/>
+        <TextArea
+          label="Brief description of your need (optional)"
+          name="needs"
+          onChange={this.setField}
+        />
+        <br/>
+        <div class="has-text-centered">
+          <button
+            class="is-danger has-text-dark button is-size-7"
+            onClick={this.submit}
+            type="submit"
+          >
+            Send →
+          </button>
+        </div>
+      </form>
+    )
+  }
+}
+
 
 export default ContactForm;
